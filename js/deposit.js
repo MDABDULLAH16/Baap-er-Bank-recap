4c959bd78c0698366723265a5973563790831bff
MD ABDULLAH <mdabdullah161036@gmail.com>
2023-12-11 13:29:21 +0600

    added deposit and update total deposit

---
+++ js/deposit.js
@@ -0,0 +1,20 @@
+//1: btn add event listener
+//2 get input value
+document.getElementById("btn-deposit").addEventListener("click", function () {
+  const getInputValue = document.getElementById("deposit-field");
+  const inputValueString = getInputValue.value;
+  const inputDepositValue = parseFloat(inputValueString);
+  getInputValue.value = "";
+  // error handling for only deposit click or nothing add something
+  if (isNaN(inputDepositValue)) {
+    alert("Please input a valid number");
+    return;
+  }
+
+  //get previous deposit and update total deposit
+  const previousDepositElement = document.getElementById("total-deposit");
+  const previousDepositString = previousDepositElement.innerText;
+  const previousDeposit = parseFloat(previousDepositString);
+  const totalDeposit = inputDepositValue + previousDeposit;
+  previousDepositElement.innerText = totalDeposit;
+});
